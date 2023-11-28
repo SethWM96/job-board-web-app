6313ea8bbbd2374a99e2a7ea2ea6b7b4bba12f51
Seth Martin <sethwmartin29@gmail.com>
2023-11-28 13:50:58 -0700

    Added UX design changes

--- src/app.py
+++ src/app.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 
-from flask import Flask, request
 from flask import Flask, request, url_for
 
 app = Flask(__name__)
@@ -12,10 +11,12 @@ def main():
     <html>
     <head>
         <title>Job Search</title>
-        <link rel="stylesheet" href="{}">
+        <link rel="stylesheet" href="static/styles.css">
     </head>
     <body>
+      <div id="input_whole_screen">
         <h1>Welcome to the Job Search Portal</h1>
+      <div id="input_whole_screen">
         <p>Please enter the type of job you are looking for:</p>
         <form action="/submit_job_type" method="POST">
             <input type="text" name="job_type" placeholder="Enter job type">
@@ -23,7 +24,7 @@ def main():
         </form>
     </body>
     </html>
-    '''.format(url_for('static', filename='styles.css'))
+    '''
 
 @app.route("/submit_job_type", methods=["POST"])
 def submit_job_type():
@@ -33,16 +34,15 @@ def submit_job_type():
     <html>
     <head>
         <title>Job Type Submitted</title>
-        <link rel="stylesheet" href="{}">
+        <link rel="stylesheet" href="static/styles.css">
     </head>
     <body>
+      <div id="input_whole_screen">
         <h1>Job Type Submitted</h1>
         <p>You entered: {user_input}</p>
         <p>Thank you for submitting your job type. We'll assist you in finding relevant jobs.</p>
     </body>
     </html>
-    '''.format(url_for('static', filename='styles.css'))
+    '''
 
-if __name__ == '__main__':
-    app.run(debug=True)
 
