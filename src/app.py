249267ee1a97369580e4a5e183ac18a5fdf68111
Seth Martin <sethwmartin29@gmail.com>
2023-11-28 12:27:11 -0700

    Revised app.py file

--- src/app.py
+++ src/app.py
@@ -7,13 +7,36 @@ app = Flask(__name__)
 @app.route("/")
 def main():
     return '''
-     <form action="/echo_user_input" method="POST">
-         <input name="user_input">
-         <input type="submit" value="Submit!">
-     </form>
-     '''
+    <!DOCTYPE html>
+    <html>
+    <head>
+        <title>Job Search</title>
+    </head>
+    <body>
+        <h1>Welcome to the Job Search Portal</h1>
+        <p>Please enter the type of job you are looking for:</p>
+        <form action="/submit_job_type" method="POST">
+            <input type="text" name="job_type" placeholder="Enter job type">
+            <input type="submit" value="Submit">
+        </form>
+    </body>
+    </html>
+    '''
 
-@app.route("/echo_user_input", methods=["POST"])
-def echo_input():
-    input_text = request.form.get("user_input", "")
-    return "You entered: " + input_text
+@app.route("/submit_job_type", methods=["POST"])
+def submit_job_type():
+    user_input = request.form.get("job_type", "")
+    return f'''
+    <!DOCTYPE html>
+    <html>
+    <head>
+        <title>Job Type Submitted</title>
+    </head>
+    <body>
+        <h1>Job Type Submitted</h1>
+        <p>You entered: {user_input}</p>
+        <p>Thank you for submitting your job type. We'll assist you in finding relevant jobs.</p>
+    </body>
+    </html>
+    '''
+
